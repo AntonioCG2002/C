4352224c66f735a98b4f5f7148b6e54f28a8fe21
Antonio Cortés <78567128+AntonioCG2002@users.noreply.github.com>
2021-05-06 10:31:42 -0500

    homework7

--- homework/homework7/header.h
+++ homework/homework7/header.h
@@ -1,3 +1,9 @@
+/*Para compilar agrega 3 archivos en el gcc compilador online con los siguientes nombres
+main.c
+function.c
+header.h
+y copia cada código del repo al compilador online, no necesitas ningún archivo de texto ya que se crea automáticamente
+EL código del admi es 0 */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
